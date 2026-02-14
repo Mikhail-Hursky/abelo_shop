bdf6c8a5080ec87f8f8ba3ba0aff6be1d688f1a0
M. Hurski <kakai747o@gmail.com>
2026-02-14 13:09:59 +0300

    update config

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
+  output: 'standalone',
   sassOptions: {
     additionalData: '@use "@/shared/styles/mixins" as *;',
   },
